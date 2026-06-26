4e4ef4ca9e64b5b08c48015b66bcfb4a1bdfd041
AlexMa77 <am9614843@gmail.com>
2026-06-26 08:46:37 -0500

    PAN-03-MODULO-DISPOSITIVOS-Y-ALERTAS: Register UserLocation in admin

--- dispositivos_alertas/admin.py
+++ dispositivos_alertas/admin.py
@@ -6,3 +6,11 @@ class UserDeviceAdmin(admin.ModelAdmin):
     list_display = ('device_name', 'user', 'operating_system', 'browser', 'last_login', 'is_trusted')
     search_fields = ('device_name', 'user__username', 'user__email')
     list_filter = ('is_trusted', 'operating_system', 'browser')
+
+from .models import UserLocation
+
+@admin.register(UserLocation)
+class UserLocationAdmin(admin.ModelAdmin):
+    list_display = ('city', 'country', 'user', 'created_at')
+    search_fields = ('city', 'country', 'user__username', 'user__email')
+    list_filter = ('country', 'created_at')
