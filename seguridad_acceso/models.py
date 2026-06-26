a0f7309e2a548bc211b64f50780c2edd30662e0c
AlexMa77 <am9614843@gmail.com>
2026-06-26 08:04:51 -0500

    PAN-01-MODULO-SEGURIDAD-Y-ACCESO: Create UserDevice model

--- seguridad_acceso/models.py
+++ seguridad_acceso/models.py
@@ -68,3 +68,19 @@ class SecurityAlert(models.Model):
 
     def __str__(self):
         return f"[{self.severity}] {self.alert_type}"
+
+class UserDevice(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='devices')
+    device_name = models.CharField(max_length=100)
+    operating_system = models.CharField(max_length=50)
+    browser = models.CharField(max_length=50)
+    last_login = models.DateTimeField()
+    is_trusted = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'user_devices'
+        verbose_name = 'User Device'
+        verbose_name_plural = 'User Devices'
+
+    def __str__(self):
+        return f"{self.device_name} ({self.user})"
